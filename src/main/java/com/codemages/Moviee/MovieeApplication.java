c9d94d618b59b6f2283c65d4a07102fa691bff81
Wesley Prado <wesleyprado.dev@gmail.com>
2025-02-15 21:13:38 -0300

    feat: Initialize Moviee project with Spring Boot setup and basic configurations

---
+++ src/main/java/com/codemages/Moviee/MovieeApplication.java
@@ -0,0 +1,13 @@
+package com.codemages.Moviee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codemages/Moviee/MovieeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codemages.Moviee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
