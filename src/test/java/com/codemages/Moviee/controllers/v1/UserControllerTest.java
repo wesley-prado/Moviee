3f8b639ffdfa0fe6260c25ae4c215b2461bc89fb
Wesley Prado <wprado1424@gmail.com>
2025-07-10 18:22:31 -0300

    feat: adiciona SSL no proxy reverso

--- src/test/java/com/codemages/Moviee/controllers/v1/UserControllerTest.java
+++ src/test/java/com/codemages/Moviee/controllers/v1/UserControllerTest.java
@@ -1,73 +1,66 @@
 package com.codemages.Moviee.controllers.v1;
 
-import static org.mockito.Mockito.never;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
-import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-import java.util.List;
-import java.util.UUID;
-
-import com.codemages.Moviee.config.DataInitializer;
+import com.codemages.Moviee.dtos.UserResponseDTO;
+import com.codemages.Moviee.entities.Role;
+import com.codemages.Moviee.entities.UserStatus;
+import com.codemages.Moviee.security.AuthContextHelper;
+import com.codemages.Moviee.services.UserService;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.test.context.support.WithMockUser;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
-import com.codemages.Moviee.dtos.UserResponseDTO;
-import com.codemages.Moviee.entities.Role;
-import com.codemages.Moviee.entities.UserStatus;
-import com.codemages.Moviee.services.UserService;
+import static com.codemages.Moviee.config.MediaTypes.DEFAULT_MEDIA_TYPE;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
-  classes = {
-    com.codemages.Moviee.controllers.v1.UserController.class,
-    com.codemages.Moviee.MovieeApplication.class
-  })
-@ActiveProfiles("test")
+import java.util.List;
+import java.util.UUID;
+
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class UserControllerTest {
-  private static final String DEFAULT_MEDIA_TYPE = "application/hal+json";
-  
-  private MockMvc mvc;
-  @MockitoBean
-  private UserService userService;
-  //  @MockitoBean
-//  private DataInitializer dataInitializer;
   @Autowired
   private WebApplicationContext context;
+  private MockMvc mvc;
 
-  private UUID userId = UUID.randomUUID();
-  private UserResponseDTO userResponseDTO = new UserResponseDTO(
-    userId, "Test User",
-    "wesleyprado.dev@gmail.com", Role.USER.name(),
-    UserStatus.ACTIVE.name()
-  );
-  private List<UserResponseDTO> userList = List.of( userResponseDTO );
+  @MockitoBean
+  private UserService userServiceMock;
+
+  @MockitoBean
+  private AuthContextHelper authContextHelperMock;
 
   @BeforeEach
-  void makeData() {
-    mvc = MockMvcBuilders.webAppContextSetup( context ).apply( springSecurity() )
-      .build();
+  public void setup() {
+    mvc = MockMvcBuilders.webAppContextSetup( context ).apply( springSecurity() ).build();
   }
+
+  List<UserResponseDTO> users = List.of(
+    new UserResponseDTO(
+      UUID.randomUUID(), "Test User", "wesley@mail.com", Role.USER.name(),
+      UserStatus.ACTIVE.name()
+    ) );
+  UUID userId = users.getFirst().id();
 
   @Test
   @WithMockUser(username = "admin", password = "Admin1#@", authorities = "ADMIN")
   void getUsers_whenUserHasAdminAuthority_shouldReturnSuccess()
     throws Exception {
-    when( userService.findAll() ).thenReturn( userList );
+    when( userServiceMock.findAll() ).thenReturn( users );
 
-    mvc.perform( get( "/api/v1/users" ).accept( DEFAULT_MEDIA_TYPE ).secure( false ) )
+    mvc.perform( get( "/api/v1/users" ).accept( DEFAULT_MEDIA_TYPE )
+        .secure( true ) )
       .andExpect( status().isOk() )
       .andExpect( content().contentType( DEFAULT_MEDIA_TYPE ) )
       .andExpect(
@@ -77,23 +70,20 @@ class UserControllerTest {
       .andExpect( jsonPath( "$._embedded.userResponseDTOes[0].username" )
         .value( "Test User" ) )
       .andExpect( jsonPath( "$._embedded.userResponseDTOes[0].email" )
-        .value( "wesleyprado.dev@gmail.com" ) );
+        .value( "wesley@mail.com" ) );
 
-    verify( userService ).findAll();
+    verify( userServiceMock ).findAll();
   }
 
   @Test
-  @WithMockUser(authorities = "ROLE_USER")
+  @WithMockUser(username = "user", password = "User1#@@", authorities = "USER")
   void getUsers_whenUserHasUserAuthority_shouldReturnForbidden()
     throws Exception {
-    when( userService.findAll() ).thenReturn( userList );
+    when( userServiceMock.findAll() ).thenReturn( users );
 
-    mvc.perform( get( "/api/v1/users" ).accept( DEFAULT_MEDIA_TYPE ).secure( false ) )
+    mvc.perform( get( "/api/v1/users" ).accept( DEFAULT_MEDIA_TYPE ).secure( true ) )
       .andExpect( status().isForbidden() );
 
-    verify( userService, never() ).findAll();
+    verify( userServiceMock, never() ).findAll();
   }
-
-  // @Test
-  // void getUser_When
 }
