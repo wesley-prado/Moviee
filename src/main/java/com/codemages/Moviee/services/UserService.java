808bd4c8f5a921522111bc878faefaae58f24dc2
Wesley Prado <wesleyprado.dev@gmail.com>
2025-07-21 11:50:11 -0300

    refactor: melhora lógica de validação e legibilidade da classe UserService

--- src/main/java/com/codemages/Moviee/services/UserService.java
+++ src/main/java/com/codemages/Moviee/services/UserService.java
@@ -13,6 +13,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Service
@@ -22,74 +23,76 @@ public class UserService {
   private final PasswordValidator passwordValidator;
 
   public UserService(
-		  UserRepository userRepository,
-		  PasswordEncoder passwordEncoder,
-		  PasswordValidator passwordValidator
+    UserRepository userRepository,
+    PasswordEncoder passwordEncoder,
+    PasswordValidator passwordValidator
   ) {
-	this.userRepository = userRepository;
-	this.passwordEncoder = passwordEncoder;
-	this.passwordValidator = passwordValidator;
+    this.userRepository = userRepository;
+    this.passwordEncoder = passwordEncoder;
+    this.passwordValidator = passwordValidator;
   }
 
   public UserResponseDTO createUser(UserCreateDTO userCreateDto) {
-	validateUser( userCreateDto );
+    validateUser( userCreateDto );
 
-	return toUserResponseDTO( userRepository.save( createUserEntity( userCreateDto ) ) );
+    return toUserResponseDTO( userRepository.save( createUserEntity( userCreateDto ) ) );
   }
 
   private void validateUser(UserCreateDTO userCreateDto) {
-	if ( !passwordValidator.isValid( userCreateDto.password() ) ) {
-	  throw new IllegalArgumentException( "Password is not valid" );
-	}
+    if ( !passwordValidator.isValid( userCreateDto.password() ) ) {
+      throw new IllegalArgumentException( "Password is not valid" );
+    }
 
-	User u = userRepository.findOptionalByEmailOrUsername(
-			userCreateDto.email(),
-			userCreateDto.username()
-	).orElse( null );
+    Optional<User> u = userRepository.findOptionalByEmailOrUsername(
+      userCreateDto.email(),
+      userCreateDto.username()
+    );
 
-	if ( u != null ) {
-	  if ( u.getEmail().equals( userCreateDto.email() ) ) {
-		throw new DuplicateUserException( "Email already registered" );
-	  }
+    if ( u.isEmpty() ) {
+      return;
+    }
 
-	  if ( u.getUsername().equals( userCreateDto.username() ) ) {
-		throw new DuplicateUserException( "Username already registered" );
-	  }
-	}
+    if ( u.get().getEmail().equals( userCreateDto.email() ) ) {
+      throw new DuplicateUserException( "Email already registered" );
+    }
+
+    if ( u.get().getUsername().equals( userCreateDto.username() ) ) {
+      throw new DuplicateUserException( "Username already registered" );
+    }
   }
 
   private User createUserEntity(UserCreateDTO userCreateDto) {
-	return new User(
-			null,
-			userCreateDto.username(),
-			userCreateDto.email(),
-			passwordEncoder.encode( userCreateDto.password() ),
-			Role.valueOf( userCreateDto.role() ),
-			userCreateDto.document(),
-			DocumentType.fromString( userCreateDto.documentType() )
-	);
+    return new User(
+      null,
+      userCreateDto.username(),
+      userCreateDto.email(),
+      passwordEncoder.encode( userCreateDto.password() ),
+      Role.valueOf( userCreateDto.role() ),
+      userCreateDto.document(),
+      DocumentType.fromString( userCreateDto.documentType() )
+    );
   }
 
   public List<UserResponseDTO> findAll() {
-	return userRepository.findAll().stream().map( this::toUserResponseDTO ).toList();
+    return userRepository.findAll().stream().map( this::toUserResponseDTO ).toList();
   }
 
   public UserResponseDTO findById(UUID id) {
-	return toUserResponseDTO( userRepository.findById( id )
-			.orElseThrow( () -> new UserNotFoundException( "User not found with id: " + id ) ) );
+    return toUserResponseDTO( userRepository.findById( id )
+      .orElseThrow( () -> new UserNotFoundException( "User not found with id: " + id ) ) );
   }
 
   private UserResponseDTO toUserResponseDTO(User u) {
-	return u == null ? null : new UserResponseDTO(
-			u.getId(),
-			u.getUsername(),
-			u.getEmail(),
-			u.getRole().name(),
-			u.getStatus().toString()
-	);
+    return new UserResponseDTO(
+      u.getId(),
+      u.getUsername(),
+      u.getEmail(),
+      u.getRole().name(),
+      u.getStatus().toString()
+    );
   }
 
   public long count() {
-	return userRepository.count();
+    return userRepository.count();
   }
 }
