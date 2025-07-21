5f5ec344479c26846c3da1f76049aa97d9386221
Wesley Prado <wesleyprado.dev@gmail.com>
2025-07-21 11:48:36 -0300

    feat: implementa exceção de client e atualiza o tratamento de erros no serviço

--- src/main/java/com/codemages/Moviee/services/ClientService.java
+++ src/main/java/com/codemages/Moviee/services/ClientService.java
@@ -1,33 +1,41 @@
 package com.codemages.Moviee.services;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Service;
-
 import com.codemages.Moviee.dtos.ClientDTO;
 import com.codemages.Moviee.entities.Client;
+import com.codemages.Moviee.exceptions.client.ClientNotFoundException;
 import com.codemages.Moviee.repositories.ClientRepository;
-
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class ClientService {
-	@Autowired
-	private final ClientRepository clientRepository;
-	@Autowired
-	private final PasswordEncoder passwordEncoder;
+  private final ClientRepository clientRepository;
+  private final PasswordEncoder passwordEncoder;
 
-	public void save(ClientDTO dto) {
-		var client = new Client();
-		client.setClientId(dto.clientId());
-		client.setClientSecret(passwordEncoder.encode(dto.clientSecret()));
-		client.setRedirectUri(dto.redirectUri());
+  public void save(ClientDTO dto) {
+    var client = new Client();
+    client.setClientId( dto.clientId() );
+    client.setClientSecret( passwordEncoder.encode( dto.clientSecret() ) );
+    client.setRedirectUri( dto.redirectUri() );
 
-		clientRepository.save(client);
-	}
+    clientRepository.save( client );
+  }
 
-	public int count() {
-		return (int) clientRepository.count();
-	}
+  public void update(ClientDTO dto) throws ClientNotFoundException {
+    var client = clientRepository.findByClientId( dto.clientId() );
+
+    if ( client == null ) {
+      throw new ClientNotFoundException( "Client with ID " + dto.clientId() + " does not exist." );
+    }
+
+    client.setClientSecret( passwordEncoder.encode( dto.clientSecret() ) );
+    client.setRedirectUri( dto.redirectUri() );
+    clientRepository.save( client );
+  }
+
+  public int count() {
+    return (int) clientRepository.count();
+  }
 }
