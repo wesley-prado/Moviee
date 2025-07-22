ceff28cb2ad0c8b64af08534db89d81ab3f1b44d
Wesley Prado <wesleyprado.dev@gmail.com>
2025-07-22 13:33:48 -0300

    refactor: Remove anotação desnecessária e melhora a formatação do código

--- src/test/java/com/codemages/Moviee/support/IntegrationTestContainerSingleton.java
+++ src/test/java/com/codemages/Moviee/support/IntegrationTestContainerSingleton.java
@@ -1,6 +1,5 @@
 package com.codemages.Moviee.support;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.DynamicPropertyRegistry;
@@ -9,20 +8,19 @@ import org.testcontainers.containers.PostgreSQLContainer;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
-@RequiredArgsConstructor
 public abstract class IntegrationTestContainerSingleton {
   static final PostgreSQLContainer<?> CONTAINER = new PostgreSQLContainer<>( "postgres:latest" );
 
   static {
-    CONTAINER.start();
+	CONTAINER.start();
   }
 
   @DynamicPropertySource
   static void setProperties(DynamicPropertyRegistry registry) {
-    registry.add( "spring.datasource.url", CONTAINER::getJdbcUrl );
-    registry.add( "spring.datasource.username", CONTAINER::getUsername );
-    registry.add( "spring.datasource.password", CONTAINER::getPassword );
-    registry.add( "spring.jpa.hibernate.ddl-auto", () -> "update" );
-    registry.add( "moviee.security.remember-me-key", () -> "remember-me-key" );
+	registry.add( "spring.datasource.url", CONTAINER::getJdbcUrl );
+	registry.add( "spring.datasource.username", CONTAINER::getUsername );
+	registry.add( "spring.datasource.password", CONTAINER::getPassword );
+	registry.add( "spring.jpa.hibernate.ddl-auto", () -> "update" );
+	registry.add( "moviee.security.remember-me-key", () -> "remember-me-key" );
   }
 }
