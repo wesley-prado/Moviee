2bf7a99a3a588111c696cd21015b5f5b3f5b6b70
Wesley Prado <wesleyprado.dev@gmail.com>
2025-08-04 15:22:42 -0300

    refactor: Melhora configuração do servidor de autorização

--- src/main/java/com/codemages/Moviee/config/AuthorizationServerConfig.java
+++ src/main/java/com/codemages/Moviee/config/AuthorizationServerConfig.java
@@ -2,48 +2,40 @@ package com.codemages.Moviee.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
-import org.springframework.http.MediaType;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer;
 import org.springframework.security.oauth2.server.authorization.settings.AuthorizationServerSettings;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
-import org.springframework.security.web.util.matcher.MediaTypeRequestMatcher;
 import org.springframework.security.web.util.matcher.RequestMatcher;
 
-import static org.springframework.security.config.Customizer.withDefaults;
-
 @Configuration
-@EnableMethodSecurity(prePostEnabled = true)
+@EnableMethodSecurity
 public class AuthorizationServerConfig {
   private static final String CUSTOM_CONSENT_PAGE_URI = "/oauth2/consent";
   private static final String CUSTOM_LOGIN_PAGE_URI = "/login";
 
   @Bean
-  @Order(1)
+  @Order(Ordered.HIGHEST_PRECEDENCE)
   SecurityFilterChain authServerFilterChain(HttpSecurity http)
     throws Exception {
-    OAuth2AuthorizationServerConfigurer authorizationServerConfigurer =
-      OAuth2AuthorizationServerConfigurer
-        .authorizationServer();
-    RequestMatcher endpointsMatcher = authorizationServerConfigurer
-      .getEndpointsMatcher();
+    var authServerConfigurer = OAuth2AuthorizationServerConfigurer.authorizationServer();
 
-    authorizationServerConfigurer.oidc( withDefaults() ).authorizationEndpoint(
-      auth -> auth.consentPage( CUSTOM_CONSENT_PAGE_URI ) );
+    authServerConfigurer.oidc( Customizer.withDefaults() )
+      .authorizationEndpoint( endpoint -> endpoint.consentPage( CUSTOM_CONSENT_PAGE_URI ) );
+
+    RequestMatcher endpointsMatcher = authServerConfigurer.getEndpointsMatcher();
 
     http.securityMatcher( endpointsMatcher )
-      .authorizeHttpRequests( authz -> authz.anyRequest().authenticated() )
+      .authorizeHttpRequests( (authorize) -> authorize.anyRequest().authenticated() )
       .csrf( csrf -> csrf.ignoringRequestMatchers( endpointsMatcher ) )
-      .with( authorizationServerConfigurer, withDefaults() );
-
-    http.exceptionHandling(
-      exceptions -> exceptions.defaultAuthenticationEntryPointFor(
-        new LoginUrlAuthenticationEntryPoint( CUSTOM_LOGIN_PAGE_URI ),
-        new MediaTypeRequestMatcher( MediaType.TEXT_HTML )
-      ) );
+      .exceptionHandling( exceptions -> exceptions.authenticationEntryPoint( new LoginUrlAuthenticationEntryPoint(
+        CUSTOM_LOGIN_PAGE_URI ) ) )
+      .with( authServerConfigurer, Customizer.withDefaults() );
 
     return http.build();
   }
@@ -53,4 +45,12 @@ public class AuthorizationServerConfig {
     return AuthorizationServerSettings.builder().issuer( "http://localhost:8080" )
       .build();
   }
+
+//  @Bean
+//  RegisteredClientRepository registeredClientRepository(
+//    @Autowired ClientRepository clientRepository,
+//    @Autowired PasswordEncoder passwordEncoder
+//  ) {
+//    return new CustomRegisteredClientRepository( clientRepository, passwordEncoder );
+//  }
 }
