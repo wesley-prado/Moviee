0573c6f35966e7156d3d9d1d5863c42638e957b9
Wesley Prado <wesleyprado.dev@gmail.com>
2025-08-04 15:17:56 -0300

    style: Melhora a formatação do método de login no LoginController

--- src/main/java/com/codemages/Moviee/controllers/v1/LoginController.java
+++ src/main/java/com/codemages/Moviee/controllers/v1/LoginController.java
@@ -1,26 +1,27 @@
 package com.codemages.Moviee.controllers.v1;
 
+import com.codemages.Moviee.models.LoginModel;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import com.codemages.Moviee.models.LoginModel;
-
 @Controller
 public class LoginController {
-	@GetMapping("/login")
-	public String login(Model model, @RequestParam(required = false) String error,
-			@RequestParam(required = false) String logout) {
-		if (error != null) {
-			model.addAttribute("error", "Invalid username or password.");
-		}
-		if (logout != null) {
-			model.addAttribute("message", "You have been logged out successfully.");
-		}
+  @GetMapping("/login")
+  public String login(
+    Model model, @RequestParam(required = false) String error,
+    @RequestParam(required = false) String logout
+  ) {
+    if ( error != null ) {
+      model.addAttribute( "error", "Invalid username or password." );
+    }
+    if ( logout != null ) {
+      model.addAttribute( "message", "You have been logged out successfully." );
+    }
 
-		model.addAttribute("loginModel", new LoginModel());
+    model.addAttribute( "loginModel", new LoginModel() );
 
-		return "login";
-	}
+    return "login";
+  }
 }
