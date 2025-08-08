18689d1e14bea83385f71a3e40de4abdc367c443
Wesley Prado <wesleyprado.dev@gmail.com>
2025-08-08 13:32:16 -0300

    refactor: Substitui LoggerFactory por Lombok no LoggingController

--- src/main/java/com/codemages/Moviee/controllers/v1/LoggingController.java
+++ src/main/java/com/codemages/Moviee/controllers/v1/LoggingController.java
@@ -1,24 +1,21 @@
 package com.codemages.Moviee.controllers.v1;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
-
 public class LoggingController {
-  Logger logger = LoggerFactory.getLogger( LoggingController.class );
-
   @PreAuthorize("hasAuthority('ADMIN')")
   @RequestMapping("/logging")
   public String logging() {
-    logger.trace( "A TRACE Message" );
-    logger.debug( "A DEBUG Message" );
-    logger.info( "An INFO Message" );
-    logger.warn( "A WARN Message" );
-    logger.error( "An ERROR Message" );
+    log.trace( "A TRACE Message" );
+    log.debug( "A DEBUG Message" );
+    log.info( "An INFO Message" );
+    log.warn( "A WARN Message" );
+    log.error( "An ERROR Message" );
 
     return "Howdy! Check out the Logs to see the output...";
   }
