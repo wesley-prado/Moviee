23066c17afd47720b8c5e017ae21ba8bba9e2811
Wesley Prado <wesleyprado.dev@gmail.com>
2025-08-15 16:40:55 -0300

    refactor: Atualiza script de entrada e simplifica configuração do Docker

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-
-while inotifywait -r -e modify /app/src/; 
-do 
-  ./mvnw compile -o -DskipTests && touch /app/.triggerfile; 
-done >/dev/null 2>&1 &
-
-./mvnw spring-boot:run -Dspring-boot.run.arguments=--logging.level.org.springframework=DEBUG
+exec "$@"
+#while inotifywait -r -e modify /app/src/; 
+#do 
+#  ./mvnw compile -o -DskipTests && touch /app/.triggerfile; 
+#done >/dev/null 2>&1 &
+#
+#./mvnw spring-boot:run -Dspring-boot.run.arguments=--logging.level.org.springframework=DEBUG
