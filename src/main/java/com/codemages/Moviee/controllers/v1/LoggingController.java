15f003187bc781d004e4bd84bb08e7a97e07c939
Wesley Prado <wesleyprado.dev@gmail.com>
2025-08-18 10:48:38 -0300

    refactor: Atualiza LoggingController para usar ResponseEntity e altera mapeamento de rota

--- src/main/java/com/codemages/Moviee/controllers/v1/LoggingController.java
+++ src/main/java/com/codemages/Moviee/controllers/v1/LoggingController.java
@@ -1,22 +1,23 @@
 package com.codemages.Moviee.controllers.v1;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
 public class LoggingController {
   @PreAuthorize("hasAuthority('ADMIN')")
-  @RequestMapping("/logging")
-  public String logging() {
+  @GetMapping("/api/v1/logging")
+  public ResponseEntity<String> logging() {
     log.trace( "A TRACE Message" );
     log.debug( "A DEBUG Message" );
     log.info( "An INFO Message" );
     log.warn( "A WARN Message" );
     log.error( "An ERROR Message" );
 
-    return "Howdy! Check out the Logs to see the output...";
+    return ResponseEntity.ok( "Howdy! Check out the Logs to see the output..." );
   }
 }
