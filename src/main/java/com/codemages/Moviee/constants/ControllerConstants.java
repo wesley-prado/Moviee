407f2f7d87abbee244c3944e66af6906ce4bb510
Wesley Prado <wesleyprado.dev@gmail.com>
2025-08-25 22:17:20 -0300

    feat: Adiciona constantes de caminhos da API no ControllerConstants

---
+++ src/main/java/com/codemages/Moviee/constants/ControllerConstants.java
@@ -0,0 +1,6 @@
+package com.codemages.Moviee.constants;
+
+public class ControllerConstants {
+  public static final String API_BASE = "/api";
+  public static final String PUBLIC_API_BASE = API_BASE + "/public";
+}
