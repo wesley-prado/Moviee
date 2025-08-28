08839fef18822269698f6089ee2cc05b6409881e
Wesley Prado <wesleyprado.dev@gmail.com>
2025-08-28 10:40:25 -0300

    feat: Renomeia controlador de papéis para PrivateRoleController e ajusta permissões

--- src/main/java/com/codemages/Moviee/user/controllers/v1/PrivateRoleController.java
+++ src/main/java/com/codemages/Moviee/user/controllers/v1/PrivateRoleController.java
@@ -1,5 +1,6 @@
 package com.codemages.Moviee.user.controllers.v1;
 
+import com.codemages.Moviee.constants.ControllerConstants;
 import com.codemages.Moviee.user.assemblers.RoleModelAssembler;
 import com.codemages.Moviee.user.dto.RoleResponseDTO;
 import com.codemages.Moviee.user.enums.Role;
@@ -17,21 +18,20 @@ import java.util.Arrays;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/roles")
+@RequestMapping(ControllerConstants.API_BASE + "/v1/roles")
 @RequiredArgsConstructor
-public class RoleController {
+public class PrivateRoleController {
   private final RoleModelAssembler roleModelAssembler;
 
   @GetMapping(produces = MediaTypes.HAL_JSON_VALUE)
-  @PreAuthorize("isAuthenticated()")
+  @PreAuthorize("hasAuthority('MODERATOR')")
   public ResponseEntity<CollectionModel<EntityModel<RoleResponseDTO>>> getRoles() {
     List<RoleResponseDTO> roles = Arrays.stream( Role.values() )
-      .map( Role::name )
-      .map( RoleResponseDTO::new )
+      .map( role -> new RoleResponseDTO( role.getDisplayName(), role.getDescription() ) )
       .toList();
 
-    return ResponseEntity.ok().contentType( MediaTypes.HAL_JSON )
-      .body( roleModelAssembler
-        .toCollectionModel( roles ) );
+    return ResponseEntity.ok()
+      .contentType( MediaTypes.HAL_JSON )
+      .body( roleModelAssembler.toCollectionModel( roles ) );
   }
 }
