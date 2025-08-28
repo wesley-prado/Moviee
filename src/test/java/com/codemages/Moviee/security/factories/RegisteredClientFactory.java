d4e778404d3cbaad8b92448eac198408615e5b3a
Wesley Prado <wesleyprado.dev@gmail.com>
2025-08-28 10:39:33 -0300

    test: Adiciona fábrica para criação de clientes registrados com configurações padrão

---
+++ src/test/java/com/codemages/Moviee/security/factories/RegisteredClientFactory.java
@@ -0,0 +1,44 @@
+package com.codemages.Moviee.security.factories;
+
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
+import org.springframework.security.oauth2.core.oidc.OidcScopes;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
+import org.springframework.security.oauth2.server.authorization.settings.ClientSettings;
+import org.springframework.security.oauth2.server.authorization.settings.TokenSettings;
+
+import java.time.Duration;
+import java.util.UUID;
+
+public class RegisteredClientFactory {
+  public static RegisteredClient createValidClient() {
+    return RegisteredClient.withId( UUID.randomUUID().toString() )
+      .clientId( UUID.randomUUID().toString() )
+      .clientSecret( "{noop}client_secret" )
+      .clientName( "Test Client" )
+      .clientAuthenticationMethods( authMethods -> {
+        authMethods.add( ClientAuthenticationMethod.CLIENT_SECRET_BASIC );
+        authMethods.add( ClientAuthenticationMethod.CLIENT_SECRET_POST );
+      } )
+      .authorizationGrantTypes( grantTypes -> {
+        grantTypes.add( AuthorizationGrantType.AUTHORIZATION_CODE );
+        grantTypes.add( AuthorizationGrantType.REFRESH_TOKEN );
+      } )
+      .redirectUri( "https://moviee.test.com/callback" )
+      .scopes( scopes -> {
+        scopes.add( OidcScopes.OPENID );
+        scopes.add( OidcScopes.PROFILE );
+        scopes.add( OidcScopes.EMAIL );
+        scopes.add( "offline_access" );
+      } )
+      .clientSettings( ClientSettings.builder()
+        .requireAuthorizationConsent( true )
+        .requireProofKey( false )
+        .build() )
+      .tokenSettings( TokenSettings.builder()
+        .accessTokenTimeToLive( Duration.ofMinutes( 30 ) )
+        .refreshTokenTimeToLive( Duration.ofDays( 7 ) )
+        .build() )
+      .build();
+  }
+}
