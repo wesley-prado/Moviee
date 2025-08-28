b55bafa1f29044fca8c7a423ca4290ede71a6ad9
Wesley Prado <wesleyprado.dev@gmail.com>
2025-08-28 10:40:03 -0300

    feat: Adiciona controlador público para acesso a filmes

---
+++ src/main/java/com/codemages/Moviee/movie/controllers/PublicMovieController.java
@@ -0,0 +1,33 @@
+package com.codemages.Moviee.movie.controllers;
+
+import com.codemages.Moviee.constants.ControllerConstants;
+import com.codemages.Moviee.movie.MovieService;
+import com.codemages.Moviee.movie.assemblers.MovieModelAssembler;
+import com.codemages.Moviee.movie.dto.MovieResponseDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.MediaTypes;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(ControllerConstants.PUBLIC_API_BASE + "/v1/movies")
+@RequiredArgsConstructor
+public class PublicMovieController {
+  private final MovieService movieService;
+  private static final MovieModelAssembler movieModelAssembler = new MovieModelAssembler();
+
+  @GetMapping
+  public ResponseEntity<CollectionModel<EntityModel<MovieResponseDTO>>> getMovies() {
+    List<MovieResponseDTO> movies = movieService.findAll();
+
+    return ResponseEntity.ok()
+      .contentType( MediaTypes.HAL_JSON )
+      .body( movieModelAssembler.toCollectionModel( movies ) );
+  }
+}
