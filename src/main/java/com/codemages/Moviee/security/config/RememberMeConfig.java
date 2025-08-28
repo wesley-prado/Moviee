a6412ce38931ecafa23bd5c836343ba0c5f8d98c
Wesley Prado <wesleyprado.dev@gmail.com>
2025-08-28 10:39:05 -0300

    feat: Adiciona configuração para "remember me" com persistência em banco de dados

---
+++ src/main/java/com/codemages/Moviee/security/config/RememberMeConfig.java
@@ -0,0 +1,38 @@
+package com.codemages.Moviee.security.config;
+
+import com.codemages.Moviee.security.config.properties.SecurityProperties;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.RememberMeConfigurer;
+import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
+
+import javax.sql.DataSource;
+
+
+@Configuration
+@RequiredArgsConstructor
+public class RememberMeConfig {
+  private static final int FOURTEEN_DAYS_IN_SECONDS = 60 * 60 * 24 * 14;
+
+  private final DataSource dataSource;
+  private final SecurityProperties securityProperties;
+
+  @Bean
+  public Customizer<RememberMeConfigurer<HttpSecurity>> getConfig() {
+    return rememberMe -> rememberMe.tokenRepository( persistentTokenRepository() )
+      .tokenValiditySeconds( FOURTEEN_DAYS_IN_SECONDS )
+      .key( securityProperties.rememberMeKey() );
+  }
+
+  @Bean
+  public PersistentTokenRepository persistentTokenRepository() {
+    JdbcTokenRepositoryImpl tokenRepository = new JdbcTokenRepositoryImpl();
+    tokenRepository.setDataSource( dataSource );
+
+    return tokenRepository;
+  }
+}
