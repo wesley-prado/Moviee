4751e9a33788416fa034b54d330b32fd99bc0a74
Wesley Prado <wesleyprado.dev@gmail.com>
2025-08-28 10:36:16 -0300

    feat: Renomeia UserController para PrivateUserController e ajusta a criação de usuários

--- src/main/java/com/codemages/Moviee/user/controllers/v1/PrivateUserController.java
+++ src/main/java/com/codemages/Moviee/user/controllers/v1/PrivateUserController.java
@@ -1,12 +1,10 @@
 package com.codemages.Moviee.user.controllers.v1;
 
-import com.codemages.Moviee.exceptions.ForbiddenOperationException;
-import com.codemages.Moviee.security.config.AuthContextHelper;
+import com.codemages.Moviee.constants.ControllerConstants;
 import com.codemages.Moviee.user.UserService;
 import com.codemages.Moviee.user.assemblers.UserModelAssembler;
-import com.codemages.Moviee.user.dto.UserCreateDTO;
+import com.codemages.Moviee.user.dto.PrivateUserCreationDTO;
 import com.codemages.Moviee.user.dto.UserResponseDTO;
-import com.codemages.Moviee.user.enums.Role;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.hateoas.CollectionModel;
@@ -21,13 +19,11 @@ import java.util.List;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/api/v1/users")
+@RequestMapping(ControllerConstants.API_BASE + "/v1/users")
 @RequiredArgsConstructor
-public class UserController {
-
+public class PrivateUserController {
   private final UserModelAssembler userModelAssembler;
   private final UserService userService;
-  private final AuthContextHelper authContextHelper;
 
   @PreAuthorize("hasAuthority('ADMIN')")
   @GetMapping(produces = MediaTypes.HAL_JSON_VALUE)
@@ -51,23 +47,12 @@ public class UserController {
 
   }
 
-  //TODO: Corrigir permissão para que um usuário anônimo possa criar uma conta
+  @PreAuthorize("hasAuthority('ADMIN')")
   @PostMapping(consumes = "application/json", produces = MediaTypes.HAL_JSON_VALUE)
   public ResponseEntity<EntityModel<UserResponseDTO>> createUser(
-    @RequestBody @Valid UserCreateDTO dto
+    @RequestBody @Valid PrivateUserCreationDTO dto
   ) {
-    List<String> specialRoles = List.of(
-      Role.ADMIN.name(),
-      Role.MODERATOR.name()
-    );
-
-    if ( specialRoles.contains( dto.role() )
-      && !authContextHelper.isUserAdmin() ) {
-      throw new ForbiddenOperationException(
-        "Only admins can create other admins or moderators." );
-    }
-
-    UserResponseDTO result = userService.createUser( dto );
+    UserResponseDTO result = userService.createPrivateUser( dto );
 
     return ResponseEntity.status( HttpStatus.CREATED )
       .contentType( MediaTypes.HAL_JSON )
