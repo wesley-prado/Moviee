073f5d47856a26a439c57ea8e833f809983007af
Wesley Prado <wesleyprado.dev@gmail.com>
2025-08-28 10:37:26 -0300

    feat: Adiciona métodos para criação de usuários públicos e privados

--- src/test/java/com/codemages/Moviee/user/factories/UserFactory.java
+++ src/test/java/com/codemages/Moviee/user/factories/UserFactory.java
@@ -2,7 +2,8 @@ package com.codemages.Moviee.user.factories;
 
 import com.codemages.Moviee.security.password.CustomPasswordGenerator;
 import com.codemages.Moviee.security.password.interfaces.PasswordGenerator;
-import com.codemages.Moviee.user.dto.UserCreateDTO;
+import com.codemages.Moviee.user.dto.PrivateUserCreationDTO;
+import com.codemages.Moviee.user.dto.PublicUserCreationDTO;
 import com.codemages.Moviee.user.dto.UserResponseDTO;
 import com.codemages.Moviee.user.enums.DocumentType;
 import com.codemages.Moviee.user.enums.Role;
@@ -12,9 +13,27 @@ import org.jetbrains.annotations.NotNull;
 import java.util.UUID;
 
 public class UserFactory {
-  private final PasswordGenerator passwordGenerator = new CustomPasswordGenerator();
+  private static final PasswordGenerator passwordGenerator = new CustomPasswordGenerator();
 
-  public UserCreateDTO createValidUserCreateDTO(@NotNull Role role) {
+  public static PublicUserCreationDTO createPublicUserDTO() {
+    UUID id = java.util.UUID.randomUUID();
+    String randomIdentifier = id.toString();
+    StringBuilder sb = new StringBuilder( "testuser_" );
+    String username = sb.append( Role.USER.getDisplayName().toLowerCase() )
+      .append( "_" )
+      .append( randomIdentifier )
+      .substring( 0, 24 );
+
+    return new PublicUserCreationDTO(
+      username,
+      username + "@test.com",
+      passwordGenerator.generate(),
+      "1234567890",
+      DocumentType.RG.name()
+    );
+  }
+
+  public static PrivateUserCreationDTO createPrivateUserDTO(@NotNull Role role) {
     UUID id = java.util.UUID.randomUUID();
     String randomIdentifier = id.toString();
     StringBuilder sb = new StringBuilder( "testuser_" );
@@ -22,22 +41,33 @@ public class UserFactory {
       .append( "_" )
       .append( randomIdentifier )
       .substring( 0, 24 );
-    return new UserCreateDTO(
+
+    return new PrivateUserCreationDTO(
       username,
       username + "@test.com",
       passwordGenerator.generate(),
       "1234567890",
       DocumentType.RG.name(),
-      role.name()
+      role
     );
   }
 
-  public UserResponseDTO createValidUserResponseDTO(UserCreateDTO dto) {
+  public static UserResponseDTO createUserResponseDTO(PublicUserCreationDTO dto) {
     return new UserResponseDTO(
       UUID.randomUUID(),
       dto.username(),
       dto.email(),
-      dto.role(),
+      Role.USER.getDisplayName(),
+      UserStatus.ACTIVE.name()
+    );
+  }
+
+  public static UserResponseDTO createUserResponseDTO(PrivateUserCreationDTO dto) {
+    return new UserResponseDTO(
+      UUID.randomUUID(),
+      dto.username(),
+      dto.email(),
+      dto.role().getDisplayName(),
       UserStatus.ACTIVE.name()
     );
   }
