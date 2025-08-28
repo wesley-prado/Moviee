85469066a80db1c4a83ffb8568a01b75d0718dc8
Wesley Prado <wesleyprado.dev@gmail.com>
2025-08-28 10:36:32 -0300

    feat: Adiciona DTO para criação de usuários privados

---
+++ src/main/java/com/codemages/Moviee/user/dto/PrivateUserCreationDTO.java
@@ -0,0 +1,26 @@
+package com.codemages.Moviee.user.dto;
+
+import com.codemages.Moviee.security.password.StrongPassword;
+import com.codemages.Moviee.user.enums.Role;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+public record PrivateUserCreationDTO(
+  @NotBlank(message = "should not be blank")
+  @Size(min = 6,
+    max = 24,
+    message = "must be between 6 and 24 characters")
+  String username,
+  @NotBlank(message = "should not be blank")
+  @Email(message = "provide a valid email")
+  String email,
+  @StrongPassword
+  String password,
+  @NotBlank(message = "should not be blank")
+  String document,
+  @NotBlank(message = "should not be blank")
+  String documentType,
+  @NotNull(message = "role must not be null") Role role
+) {}
