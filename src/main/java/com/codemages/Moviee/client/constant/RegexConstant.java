dd164d1da3d7b54b9583f4b72e23d5ebd4c8cbe1
Wesley Prado <wesleyprado.dev@gmail.com>
2025-08-28 16:39:57 -0300

    refactor: Renomeia classe RegexConstants para RegexConstant

--- src/main/java/com/codemages/Moviee/client/constant/RegexConstant.java
+++ src/main/java/com/codemages/Moviee/client/constant/RegexConstant.java
@@ -1,7 +1,7 @@
 package com.codemages.Moviee.client.constant;
 
-public final class RegexConstants {
-  private RegexConstants() {}
+public final class RegexConstant {
+  private RegexConstant() {}
 
   public static final String URL_PATTERN = "^(http|https):\\/\\/[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,6}" +
     "(\\/\\S*)?$";
