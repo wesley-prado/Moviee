1bda4c7ca9770facfef1d701ce4e368be4627b5b
Wesley Prado <wesleyprado.dev@gmail.com>
2025-08-28 10:40:14 -0300

    feat: Adiciona controlador público para acesso a gêneros

---
+++ src/main/java/com/codemages/Moviee/movie/controllers/PublicGenreController.java
@@ -0,0 +1,32 @@
+package com.codemages.Moviee.movie.controllers;
+
+import com.codemages.Moviee.constants.ControllerConstants;
+import com.codemages.Moviee.movie.GenreService;
+import com.codemages.Moviee.movie.assemblers.GenreModelAssembler;
+import com.codemages.Moviee.movie.dto.GenreResponseDTO;
+import com.codemages.Moviee.movie.enums.Genre;
+import lombok.AllArgsConstructor;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.MediaTypes;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(ControllerConstants.PUBLIC_API_BASE + "/v1/genres")
+@AllArgsConstructor
+public class PublicGenreController {
+  private final GenreService genreService;
+  private final GenreModelAssembler genreModelAssembler = new GenreModelAssembler();
+
+  @GetMapping
+  public ResponseEntity<CollectionModel<EntityModel<GenreResponseDTO>>> getGenres() {
+    return ResponseEntity.ok()
+      .contentType( MediaTypes.HAL_JSON )
+      .body( genreModelAssembler.toCollectionModel(
+        genreService.convertToGenreResponseDTO( Genre.values() )
+      ) );
+  }
+}
