5c738bc8fa86fefbb2b6d6e4c4db0f626154a627
Wesley Prado <wesleyprado.dev@gmail.com>
2025-08-28 10:35:25 -0300

    feat: Adiciona suporte a HATEOAS no UserResponseDTO

--- src/main/java/com/codemages/Moviee/user/dto/UserResponseDTO.java
+++ src/main/java/com/codemages/Moviee/user/dto/UserResponseDTO.java
@@ -1,7 +1,10 @@
 package com.codemages.Moviee.user.dto;
 
+import org.springframework.hateoas.server.core.Relation;
+
 import java.util.UUID;
 
+@Relation(value = "user", collectionRelation = "users")
 public record UserResponseDTO(UUID id, String username, String email,
                               String role, String status) {
 
