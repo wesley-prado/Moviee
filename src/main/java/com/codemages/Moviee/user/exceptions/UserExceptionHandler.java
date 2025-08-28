fcf95b37d391ca68edd615199ece85197a824226
Wesley Prado <wesleyprado.dev@gmail.com>
2025-08-28 10:35:05 -0300

    refactor: Atualiza UserExceptionHandler para usar PrivateUserController e simplifica tratamento de exceções

--- src/main/java/com/codemages/Moviee/user/exceptions/UserExceptionHandler.java
+++ src/main/java/com/codemages/Moviee/user/exceptions/UserExceptionHandler.java
@@ -1,107 +1,58 @@
 package com.codemages.Moviee.user.exceptions;
 
 import com.codemages.Moviee.exceptions.ErrorResponse;
-import com.codemages.Moviee.user.controllers.v1.UserController;
+import com.codemages.Moviee.user.controllers.v1.PrivateUserController;
 import org.springframework.dao.DataIntegrityViolationException;
-import org.springframework.hateoas.Link;
-import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
-@RestControllerAdvice(basePackageClasses = UserController.class)
+@RestControllerAdvice(basePackageClasses = PrivateUserController.class)
 public class UserExceptionHandler {
   @ExceptionHandler(UserException.class)
-  public ResponseEntity<ErrorResponse> handleUserException(UserException e) {
-	return ResponseEntity.status( HttpStatus.BAD_REQUEST )
-			.contentType( MediaTypes.HAL_JSON )
-			.body( new ErrorResponse(
-					HttpStatus.BAD_REQUEST.value(), e.getMessage(),
-					LocalDateTime.now(), null
-			) );
+  public ResponseEntity<ErrorResponse> handleUserException(UserException ex) {
+    return ErrorResponse.create( ex, HttpStatus.BAD_REQUEST, null );
   }
 
   @ExceptionHandler(MethodArgumentNotValidException.class)
   public ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(
-		  MethodArgumentNotValidException e
+    MethodArgumentNotValidException ex
   ) {
-	List<String> errors = e.getBindingResult().getFieldErrors().stream()
-			.map( error -> error.getField() + ": " + error.getDefaultMessage() )
-			.toList();
-	var error = new ErrorResponse(
-			HttpStatus.BAD_REQUEST.value(),
-			"Field validation error", LocalDateTime.now(), errors
-	);
-
-	error.add( Link.of( "https://my-api-docs.com/errors/validations", "about" ) );
-
-	return ResponseEntity.status( HttpStatus.BAD_REQUEST )
-			.contentType( MediaTypes.HAL_JSON )
-			.body( error );
+    List<String> errors = ex.getBindingResult().getFieldErrors().stream()
+      .map( error -> error.getField() + ": " + error.getDefaultMessage() )
+      .toList();
+    return ErrorResponse.create( ex, HttpStatus.BAD_REQUEST, errors );
   }
 
   @ExceptionHandler(DuplicateUserException.class)
   public ResponseEntity<ErrorResponse> handleDuplicateUserException(
-		  DuplicateUserException e
+    DuplicateUserException ex
   ) {
-	var error = new ErrorResponse(
-			HttpStatus.CONFLICT.value(), e.getMessage(),
-			LocalDateTime.now(), null
-	);
-	error
-			.add( Link.of( "https://my-api-docs.com/errors/duplicated-user", "about" ) );
-
-	return ResponseEntity.status( HttpStatus.CONFLICT )
-			.contentType( MediaTypes.HAL_JSON )
-			.body( error );
+    return ErrorResponse.create( ex, HttpStatus.CONFLICT, null );
   }
 
   @ExceptionHandler(UserNotFoundException.class)
   public ResponseEntity<ErrorResponse> handleUserNotFoundException(
-		  UserNotFoundException e
+    UserNotFoundException ex
   ) {
-	var error = new ErrorResponse(
-			HttpStatus.NOT_FOUND.value(), e.getMessage(),
-			LocalDateTime.now(), null
-	);
-	error.add( Link.of( "https://my-api-docs.com/errors/user-not-found", "about" ) );
-
-	return ResponseEntity.status( HttpStatus.NOT_FOUND )
-			.contentType( MediaTypes.HAL_JSON )
-			.body( error );
+    return ErrorResponse.create( ex, HttpStatus.NOT_FOUND, null );
   }
 
   @ExceptionHandler(UserNotCreatedException.class)
   public ResponseEntity<ErrorResponse> handleUserNotCreatedException(
-		  UserNotCreatedException e
+    UserNotCreatedException ex
   ) {
-	var error = new ErrorResponse(
-			HttpStatus.BAD_REQUEST.value(), e.getMessage(),
-			LocalDateTime.now(), null
-	);
-	error.add( Link.of( "https://my-api-docs.com/errors/user-not-created", "about" ) );
-
-	return ResponseEntity.status( HttpStatus.BAD_REQUEST )
-			.contentType( MediaTypes.HAL_JSON )
-			.body( error );
+    return ErrorResponse.create( ex, HttpStatus.BAD_REQUEST, null );
   }
 
   @ExceptionHandler(DataIntegrityViolationException.class)
   public ResponseEntity<ErrorResponse> handleDataIntegrityViolationException(
-		  DataIntegrityViolationException e
+    DataIntegrityViolationException ex
   ) {
-	var error = new ErrorResponse(
-			HttpStatus.BAD_REQUEST.value(), "Data integrity violation: " + e.getMessage(),
-			LocalDateTime.now(), null
-	);
-	error.add( Link.of( "https://my-api-docs.com/errors/data-integrity-violation", "about" ) );
-	return ResponseEntity.status( HttpStatus.BAD_REQUEST )
-			.contentType( MediaTypes.HAL_JSON )
-			.body( error );
+    return ErrorResponse.create( ex, HttpStatus.BAD_REQUEST, null );
   }
 }
