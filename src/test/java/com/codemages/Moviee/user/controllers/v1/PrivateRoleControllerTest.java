a98d78e30a1db97abe3c2c37f9d7f2bf91b9c9ca
Wesley Prado <wesleyprado.dev@gmail.com>
2025-08-28 10:40:32 -0300

    feat: Renomeia RoleControllerTest para PrivateRoleControllerTest

--- src/test/java/com/codemages/Moviee/user/controllers/v1/PrivateRoleControllerTest.java
+++ src/test/java/com/codemages/Moviee/user/controllers/v1/PrivateRoleControllerTest.java
@@ -24,9 +24,9 @@ import static org.springframework.security.test.web.servlet.setup.SecurityMockMv
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@WebMvcTest(RoleController.class)
+@WebMvcTest(PrivateRoleController.class)
 @Import(RoleControllerTestConfig.class)
-public class RoleControllerTest {
+public class PrivateRoleControllerTest {
   private MockMvc mvc;
 
   @Autowired
