262fe513cc15319b788f9c0760b9c1c7e5f87a95
Wesley Prado <wesleyprado.dev@gmail.com>
2025-09-05 17:05:35 -0300

    feat(room): adiciona método para verificar existência de sala pelo nome

--- src/main/java/com/codemages/Moviee/cinema/room/RoomRepository.java
+++ src/main/java/com/codemages/Moviee/cinema/room/RoomRepository.java
@@ -3,4 +3,5 @@ package com.codemages.Moviee.cinema.room;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RoomRepository extends JpaRepository<Room, Long> {
+  boolean existsByName(String name);
 }
